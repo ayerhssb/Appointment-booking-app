68833bc8b54f1b50ad97c9da6ac5a3a28f95f343
Samarth Vaidya <v.samarth1212@gmail.com>
2024-10-05 01:53:38 +0530

    Remove node_modules

--- backend/node_modules/stripe/cjs/resources/Billing/MeterEvents.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// File generated from our OpenAPI spec
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MeterEvents = void 0;
-const StripeResource_js_1 = require("../../StripeResource.js");
-const stripeMethod = StripeResource_js_1.StripeResource.method;
-exports.MeterEvents = StripeResource_js_1.StripeResource.extend({
-    create: stripeMethod({ method: 'POST', fullPath: '/v1/billing/meter_events' }),
-});
